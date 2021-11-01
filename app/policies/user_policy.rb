028e6f70a216d9bd899ba843ab95661565c758b7
Kazuya-Sakashita <kz0508@gmail.com>
2021-11-01 19:41:10 +0900

    first commit

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,33 @@
+# userpolicy.rbを下記の内容にて新規に作成
+# 基本的に全ての機能を許可。
+# ただしユーザー詳細は管理ユーザーのみ許可
+class UserPolicy < ApplicationPolicy
+    def index?
+      true
+    end
+  
+    def show?
+      # ユーザー詳細は管理ユーザーのみ許可
+      user.admin?
+    end
+  
+    def create?
+      true
+    end
+  
+    def new?
+      create?
+    end
+  
+    def update?
+      true
+    end
+  
+    def edit?
+      update?
+    end
+  
+    def destroy?
+      true
+    end
+  end

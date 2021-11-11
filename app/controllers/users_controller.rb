561ea16e6b8cd8fa5911735da61d6c88916176f8
Kazuya-Sakashita <kz0508@gmail.com>
2021-11-11 19:54:54 +0900

    ログイン時の画面遷移admin/user分別

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
                User.all
              else
                # generalは、自分のを表示
-               redirect_to user_path(current_user.id)
+               redirect_to mypages_path(current_user.id)
                @user = current_user
                authorize @user
              end
